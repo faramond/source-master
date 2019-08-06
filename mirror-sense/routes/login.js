d9482f46ca66e9f5f25495b0b9eace33b5eba6dd
shubham-faramond <52782395+shubham-faramond@users.noreply.github.com>
2019-08-06 13:25:03 +0530

    Revert "Revert "stash""
    This reverts commit d7a8f4a8706c68734a42bace61b28cc17b176ba1.

--- mirror-sense/routes/login.js
+++ mirror-sense/routes/login.js
@@ -1,4 +1,5 @@
 const { Customer, validate } = require('../models/customer');
+const { Employee, validateEmp } = require('../models/employee');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
@@ -71,21 +72,56 @@ router.post('/', async (req, res) => {
         console.log('Login Post', err.message)
     }
 });
-router.post('/emp', async (req, res) => {
+
+
+
+router.get('/employee', async (req, res) => {
+    try {
+        const employee = await Employee.find().or([{ mobileNumber: req.query.mobileNumber }])
+            .sort('name');
+        res.send(employee);
+    }
+    catch (err) {
+        res.send({ 'message': err.message });
+        console.log('login Get', err.message)
+    }
+
+});
+router.patch('/employee/:id', async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+        const employee = await Employee.findByIdAndUpdate(req.params.id,
+            {
+                password: req.body.password,
+                updated: new Date(),
+
+            }, { new: true });
+
+        if (!employee) return res.status(404).send({ 'message': 'Login not found.' });
+
+        res.send(employee);
+    }
+    catch (err) {
+        res.send({ 'message': err.message });
+        console.log('Employee Patch', err.message)
+    }
+});
+router.post('/employee', async (req, res) => {
     try {
         const { error } = validate(req.body);
         if (error) return res.status(400).send(
             { 'message': error.details[0].message });
 
-        let user = await Customer.findOne({ mobileNumber: req.body.mobileNumber });
-        if (user) return res.status(400).send({ 'message': 'MobileNumber already registered.' });
+        let employee= await Employee.findOne({ mobileNumber: req.body.mobileNumber });
+        if (employee) return res.status(400).send({ 'message': 'MobileNumber already registered.' });
         req.body.email = req.body.email === undefined ? req.body.mobileNumber + '@null_email' : req.body.email;
-        user = new Customer(_.pick(req.body, ['fullName', 'mobileNumber', 'countryCode', 'password', 'email']));
+        employee = new Employee(_.pick(req.body, ['fullName', 'mobileNumber', 'countryCode', 'password', 'email']));
         const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(user.password, salt);
-        console.log(user.password);
-        await user.save();
-        res.status(201).send(_.pick(user, ['_id', 'countryCode', 'mobileNumber', 'fullName', 'email']));
+        employee.password = await bcrypt.hash(employee.password, salt);
+        console.log(employee.password);
+        await employee.save();
+        res.status(201).send(_.pick(employee, ['_id', 'countryCode', 'mobileNumber', 'fullName', 'email']));
 
 
     }
--- mirror-sense/startup/routes.js
+++ mirror-sense/startup/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const customers = require('../routes/customers');
+// const employee = require('../routes/employee');
 const bookings = require('../routes/bookings');
 const services = require('../routes/services');
 const salons = require('../routes/salons');
@@ -14,6 +15,7 @@ const users = require('../controllers/users.js');
 module.exports = function(app) {
   app.use(express.json());
   app.use('/mirror/api/customers', customers);
+  // app.use('/mirror/api/employee', employee);
   app.use('/mirror/api/bookings', bookings);
   app.use('/mirror/api/services', services);
   app.use('/mirror/api/salons', salons);
